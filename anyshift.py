84649b0aaf0f56d24e54cbaa4448af4811136688
unknown <bertoalarcon@gmail.com>
2023-05-17 18:09:11 +0200

    style and comments

--- anyshift.py
+++ anyshift.py
@@ -1,17 +1,21 @@
 """Main Anyshift executable"""
-import os
+
+import os  # Hide pygame welcome message
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
-import pygame
-import keyboard
-import configparser
-import time
-import ctypes
+import pygame  # Joystick input
+import keyboard  # Key presses
+import configparser  # Read ini files
+import time  # Delays
+import ctypes  # Kernel level key presses
+
 
 # Bunch of stuff so that the script can send keystrokes to game #
 SendInput = ctypes.windll.user32.SendInput
 
-# C struct redefinitions 
+# C struct redefinitions
 PUL = ctypes.POINTER(ctypes.c_ulong)
+
+
 class KeyBdInput(ctypes.Structure):
     _fields_ = [("wVk", ctypes.c_ushort),
                 ("wScan", ctypes.c_ushort),
@@ -19,33 +23,39 @@ class KeyBdInput(ctypes.Structure):
                 ("time", ctypes.c_ulong),
                 ("dwExtraInfo", PUL)]
 
+
 class HardwareInput(ctypes.Structure):
     _fields_ = [("uMsg", ctypes.c_ulong),
                 ("wParamL", ctypes.c_short),
                 ("wParamH", ctypes.c_ushort)]
+
 
 class MouseInput(ctypes.Structure):
     _fields_ = [("dx", ctypes.c_long),
                 ("dy", ctypes.c_long),
                 ("mouseData", ctypes.c_ulong),
                 ("dwFlags", ctypes.c_ulong),
-                ("time",ctypes.c_ulong),
+                ("time", ctypes.c_ulong),
                 ("dwExtraInfo", PUL)]
+
 
 class Input_I(ctypes.Union):
     _fields_ = [("ki", KeyBdInput),
-                 ("mi", MouseInput),
-                 ("hi", HardwareInput)]
+                ("mi", MouseInput),
+                ("hi", HardwareInput)]
+
 
 class Input(ctypes.Structure):
     _fields_ = [("type", ctypes.c_ulong),
                 ("ii", Input_I)]
+
 
 # Global variables
 up_key = 's'
 down_key = 'z'
 presskey_timer = 0
 releasekey_timer = 0
+
 
 def main():
 
@@ -59,48 +69,47 @@ def main():
     second = int(config['SHIFTER']['second gear'])
     third = int(config['SHIFTER']['third gear'])
     fourth = int(config['SHIFTER']['fourth gear'])
-    fifth = int(config['SHIFTER']['fifth gear']) 
+    fifth = int(config['SHIFTER']['fifth gear'])
     sixth = int(config['SHIFTER']['sixth gear'])
     seventh = int(config['SHIFTER']['seventh gear'])
     reverse = int(config['SHIFTER']['reverse button'])
     neut_key = config['KEYS']['neutral keyboard key']
     global up_key
     up_key = config['KEYS']['upshift']
-    global down_key 
+    global down_key
     down_key = config['KEYS']['downshift']
     global presskey_timer
     presskey_timer = float(config['OPTIONS']['presskey timer'])
     global releasekey_timer
     releasekey_timer = float(config['OPTIONS']['releasekey timer'])
-    
-    
+
     # Initialize joystick module
     pygame.joystick.init()
-    pygame.init()   
-        
+    pygame.init()
+
     # Create a joystick object and initialize it
     shifter = pygame.joystick.Joystick(int(joy_id))
     shifter.init()
-    
-    # Cool window design ;)    
+
+    # Cool window design ;)
     print()
     print("    ___                _____ __    _ ______              ___  ____  ")
     print("   /   |  ____  __  __/ ___// /_  (_) __/ /_   _   __   <  / / __ \ ")
     print("  / /| | / __ \/ / / /\__ \/ __ \/ / /_/ __/  | | / /   / / / / / / ")
     print(" / ___ |/ / / / /_/ /___/ / / / / / __/ /_    | |/ /   / /_/ /_/ /  ")
     print("/_/  |_/_/ /_/\__, //____/_/ /_/_/_/  \__/    |___/   /_/(_)____/   ")
-    print("             /____/                           ©2022 Menkaura Soft   ")    
+    print("             /____/                           ©2022 Menkaura Soft   ")
     print()
     print("Buy me a coffe if you like this app: https://bmc.link/Menkaura")
     print()
     print("Active shifter: ", shifter.get_name())
-        
+
     gear_selected = 0
     actual_gear = 0
-        
+
+    # Joystick read loop
     done = False
     while not done:
-        # Event processing step.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True  # Flag that we are done so we exit this loop.
@@ -127,10 +136,10 @@ def main():
                 if shifter.get_button(seventh) == True and seven_gears == 'True':
                     gear_selected = 7
                     actual_gear = update_gear(gear_selected, actual_gear)
-                if shifter.get_button(reverse) == True:                     
+                if shifter.get_button(reverse) == True:
                     gear_selected = -1
-                    actual_gear = update_gear(gear_selected, actual_gear)        
-                print(f"Gear in joystick: {gear_selected} -- Actual gear: {actual_gear}   ",  end="\r") 
+                    actual_gear = update_gear(gear_selected, actual_gear)
+                print(f"Gear in joystick: {gear_selected} -- Actual gear: {actual_gear}   ",  end="\r")
 
             # Change to neutral if the option is enabled. The program sleeps, and then check if the next event is a joybuttondown, if true skips neutral
             if event.type == pygame.JOYBUTTONUP and neutral == 'True':
@@ -140,59 +149,61 @@ def main():
                     actual_gear = update_gear(gear_selected, actual_gear)
                     print(f"Gear in joystick: {gear_selected} -- Actual gear: {actual_gear}   ",  end="\r")
 
-        # Select neutral if this key is pressed 
-        if keyboard.is_pressed(neut_key):  
+        # Select neutral if this key is pressed
+        if keyboard.is_pressed(neut_key):
             gear_selected = 0
             actual_gear = update_gear(gear_selected, actual_gear)
             print(f"Gear in joystick: {gear_selected} -- Actual gear: {actual_gear}   ",  end="\r")
 
-       
+
+# Function to apply sequential logic to h-shifter inputs, and make the necessary key presses
 def update_gear(gear_selected, actual_gear):
-    
+
     act_gear = actual_gear
     while act_gear != gear_selected:
         if act_gear < gear_selected:
             act_gear += 1
             KeyPress_up()
-            #keyboard.press_and_release(up_key)  # Deprecated method to send key strokes
-            #time.sleep(0.25)  # Deprecated method to send key
+            # keyboard.press_and_release(up_key)  # Deprecated method to send key strokes
+            # time.sleep(0.25)  # Deprecated method to send key
         if act_gear > gear_selected:
             act_gear -= 1
             KeyPress_down()
-            #keyboard.press_and_release(down_key)  # Deprecated method to send key strokes
-            #time.sleep(0.25)  #  Deprecated method to send key strokes
-    
+            # keyboard.press_and_release(down_key)  # Deprecated method to send key strokes
+            # time.sleep(0.25)  #  Deprecated method to send key strokes
+
     return act_gear
 
 
+# Ctypes complicated stuff for low level key presses
 def PressKey(hexKeyCode):
     extra = ctypes.c_ulong(0)
     ii_ = Input_I()
-    ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008, 0, ctypes.pointer(extra) )
-    x = Input( ctypes.c_ulong(1), ii_ )
+    ii_.ki = KeyBdInput(0, hexKeyCode, 0x0008, 0, ctypes.pointer(extra))
+    x = Input(ctypes.c_ulong(1), ii_)
     ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
 
 
 def ReleaseKey(hexKeyCode):
     extra = ctypes.c_ulong(0)
     ii_ = Input_I()
-    ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008 | 0x0002, 0, ctypes.pointer(extra) )
-    x = Input( ctypes.c_ulong(1), ii_ )
+    ii_.ki = KeyBdInput(0, hexKeyCode, 0x0008 | 0x0002, 0, ctypes.pointer(extra))
+    x = Input(ctypes.c_ulong(1), ii_)
     ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
 
 
 def KeyPress_up():
     time.sleep(presskey_timer)
-    PressKey(int(up_key, 16)) # press 
+    PressKey(int(up_key, 16))  # press
     time.sleep(releasekey_timer)
-    ReleaseKey(int(up_key, 16)) #release 
+    ReleaseKey(int(up_key, 16))  # release
 
 
 def KeyPress_down():
     time.sleep(presskey_timer)
-    PressKey(int(down_key, 16)) # press 
+    PressKey(int(down_key, 16))  # press
     time.sleep(releasekey_timer)
-    ReleaseKey(int(down_key, 16)) #release 
+    ReleaseKey(int(down_key, 16))  # release
 
 
 if __name__ == "__main__":
--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 """Config app. This will write an .ini file with the desired config of AnyShift"""
-import os
+
+import os  # Removal of pygame message
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame  # Joystick support
 import configparser  # Read and write ini files
@@ -8,58 +9,57 @@ from sys import exit  # Finish execution in some cases
 
 # Dictionary for converting input keys to hex values
 keys = {
-'1': '0x02',
-'2': '0x03',
-'3': '0x04',
-'4': '0x05',
-'5': '0x06',
-'6': '0x07',
-'7': '0x08',
-'8': '0x09',
-'9': '0x0A',
-'0': '0x0B',
-'q': '0x10',
-'w': '0x11',		
-'e': '0x12',	
-'r': '0x13',	
-'t': '0x14',	
-'y': '0x15',	
-'u': '0x16',	
-'i': '0x17',	
-'o': '0x18',	
-'p': '0x19',	
-'a': '0x1E',	
-'s': '0x1F',	
-'d': '0x20',	
-'f': '0x21',	
-'g': '0x22',	
-'h': '0x23',	
-'j': '0x24',	
-'k': '0x25',	
-'l': '0x26',	
-'z': '0x2C',	
-'x': '0x2D',	
-'c': '0x2E',	
-'v': '0x2F',	
-'b': '0x30',	
-'n': '0x31',	
-'m': '0x32'	
+    '1': '0x02',
+    '2': '0x03',
+    '3': '0x04',
+    '4': '0x05',
+    '5': '0x06',
+    '6': '0x07',
+    '7': '0x08',
+    '8': '0x09',
+    '9': '0x0A',
+    '0': '0x0B',
+    'q': '0x10',
+    'w': '0x11',
+    'e': '0x12',
+    'r': '0x13',
+    't': '0x14',
+    'y': '0x15',
+    'u': '0x16',
+    'i': '0x17',
+    'o': '0x18',
+    'p': '0x19',
+    'a': '0x1E',
+    's': '0x1F',
+    'd': '0x20',
+    'f': '0x21',
+    'g': '0x22',
+    'h': '0x23',
+    'j': '0x24',
+    'k': '0x25',
+    'l': '0x26',
+    'z': '0x2C',
+    'x': '0x2D',
+    'c': '0x2E',
+    'v': '0x2F',
+    'b': '0x30',
+    'n': '0x31',
+    'm': '0x32'
 }
 
 # Initialize joystick module
 pygame.joystick.init()
-pygame.init()  
+pygame.init()
 
 # Number of joystick connected to the pc
 num_joy = pygame.joystick.get_count()
 if num_joy == 0:
-    print("No joysticks connected to the pc") 
+    print("No joysticks connected to the pc")
     print("Please connect joystick an then run the config util again")
     time.sleep(5)
     exit(1)
-else: 
+else:
     print(f"There are {num_joy} joysticks conected to the pc")
-    
 
 # Print every joystick and its id
 for i in range(num_joy):
@@ -70,124 +70,117 @@ for i in range(num_joy):
 # Promp user for desired joystick
 joy = '111'
 while len(joy) != 1 or (ord(joy) < 48 or ord(joy) > 57) or int(joy) > num_joy:
-     joy = input("Wich joystick do you want to use as shifter?: ")
+    joy = input("Wich joystick do you want to use as shifter?: ")
 joy = int(joy)
 
 # Initialize selected joystick
 shifter = pygame.joystick.Joystick(int(joy))
 shifter.init()
-
 num_buttons = shifter.get_numbuttons()  # For the range of the loop when selecting buttons
+reserved = []  # For storing already used buttons
 
-reserved = []
 # Selection for first gear
 print("Put the shifter in position for first gear")
 done = False
 while not done:
-        # Event processing step.
-        for event in pygame.event.get():
-            if event.type == pygame.JOYBUTTONDOWN:
-                for i in range(num_buttons):
-                    if shifter.get_button(i) == True:
-                        first_gear = i
-                        reserved.append(i)
-                        print(f"Button {first_gear} saved for first gear")
-                        done = True
+    for event in pygame.event.get():
+        if event.type == pygame.JOYBUTTONDOWN:
+            for i in range(num_buttons):
+                if shifter.get_button(i) == True:
+                    first_gear = i
+                    reserved.append(i)
+                    print(f"Button {first_gear} saved for first gear")
+                    done = True
 
 # Selection for second gear
 print("Put the shifter in position for second gear")
 done = False
 while not done:
-        # Event processing step.
-        for event in pygame.event.get():
-            if event.type == pygame.JOYBUTTONDOWN:
-                for i in range(num_buttons):
-                    if shifter.get_button(i) == True:
-                        if i not in reserved:
-                            second_gear = i
-                            reserved.append(i)
-                            print(f"Button {second_gear} saved for second gear")
-                            done = True
-                        else:
-                            print(f"Conflict detected button already in use: {reserved}")
-                            print("Put the shifter in position for second gear")
-                            break
-                    
+    for event in pygame.event.get():
+        if event.type == pygame.JOYBUTTONDOWN:
+            for i in range(num_buttons):
+                if shifter.get_button(i) == True:
+                    if i not in reserved:
+                        second_gear = i
+                        reserved.append(i)
+                        print(f"Button {second_gear} saved for second gear")
+                        done = True
+                    else:
+                        print(f"Conflict detected button already in use: {reserved}")
+                        print("Put the shifter in position for second gear")
+                        break
+
 # Selection for third gear
 print("Put the shifter in position for third gear")
 done = False
 while not done:
-        # Event processing step.
-        for event in pygame.event.get():
-            if event.type == pygame.JOYBUTTONDOWN:
-                for i in range(num_buttons):
-                    if shifter.get_button(i) == True:
-                        if i not in reserved:
-                            third_gear = i
-                            reserved.append(i)
-                            print(f"Button {third_gear} saved for third gear")
-                            done = True
-                        else:
-                            print(f"Conflict detected button already in use: {reserved}")
-                            print("Put the shifter in position for third gear")
-                            break
+    for event in pygame.event.get():
+        if event.type == pygame.JOYBUTTONDOWN:
+            for i in range(num_buttons):
+                if shifter.get_button(i) == True:
+                    if i not in reserved:
+                        third_gear = i
+                        reserved.append(i)
+                        print(f"Button {third_gear} saved for third gear")
+                        done = True
+                    else:
+                        print(f"Conflict detected button already in use: {reserved}")
+                        print("Put the shifter in position for third gear")
+                        break
 
 # Selection for fourth gear
 print("Put the shifter in position for fourth gear")
 done = False
 while not done:
-        # Event processing step.
-        for event in pygame.event.get():
-            if event.type == pygame.JOYBUTTONDOWN:
-                for i in range(num_buttons):
-                    if shifter.get_button(i) == True:
-                        if i not in reserved:
-                            fourth_gear = i
-                            reserved.append(i)
-                            print(f"Button {fourth_gear} saved for fourth gear")
-                            done = True
-                        else:
-                            print(f"Conflict detected button already in use: {reserved}")
-                            print("Put the shifter in position for fourth gear")
-                            break
+    for event in pygame.event.get():
+        if event.type == pygame.JOYBUTTONDOWN:
+            for i in range(num_buttons):
+                if shifter.get_button(i) == True:
+                    if i not in reserved:
+                        fourth_gear = i
+                        reserved.append(i)
+                        print(f"Button {fourth_gear} saved for fourth gear")
+                        done = True
+                    else:
+                        print(f"Conflict detected button already in use: {reserved}")
+                        print("Put the shifter in position for fourth gear")
+                        break
 
 # Selection for fifth gear
 print("Put the shifter in position for fifth gear")
 done = False
 while not done:
-        # Event processing step.
-        for event in pygame.event.get():
-            if event.type == pygame.JOYBUTTONDOWN:
-                for i in range(num_buttons):
-                    if shifter.get_button(i) == True:
-                        if i not in reserved:
-                            fifth_gear = i
-                            reserved.append(i)
-                            print(f"Button {fifth_gear} saved for fifth gear")
-                            done = True
-                        else:
-                            print(f"Conflict detected button already in use: {reserved}")
-                            print("Put the shifter in position for fifth gear")
-                            break
+    for event in pygame.event.get():
+        if event.type == pygame.JOYBUTTONDOWN:
+            for i in range(num_buttons):
+                if shifter.get_button(i) == True:
+                    if i not in reserved:
+                        fifth_gear = i
+                        reserved.append(i)
+                        print(f"Button {fifth_gear} saved for fifth gear")
+                        done = True
+                    else:
+                        print(f"Conflict detected button already in use: {reserved}")
+                        print("Put the shifter in position for fifth gear")
+                        break
 
 # Selection for sixth gear
 print("Put the shifter in position for sixth gear")
 done = False
 while not done:
-        # Event processing step.
-        for event in pygame.event.get():
-            if event.type == pygame.JOYBUTTONDOWN:
-                for i in range(num_buttons):
-                    if shifter.get_button(i) == True:
-                        if i not in reserved:
-                            sixth_gear = i
-                            reserved.append(i)
-                            print(f"Button {sixth_gear} saved for sixth gear")
-                            done = True
-                        else:
-                            print(f"Conflict detected button already in use: {reserved}")
-                            print("Put the shifter in position for sixth gear")
-                            break
+    for event in pygame.event.get():
+        if event.type == pygame.JOYBUTTONDOWN:
+            for i in range(num_buttons):
+                if shifter.get_button(i) == True:
+                    if i not in reserved:
+                        sixth_gear = i
+                        reserved.append(i)
+                        print(f"Button {sixth_gear} saved for sixth gear")
+                        done = True
+                    else:
+                        print(f"Conflict detected button already in use: {reserved}")
+                        print("Put the shifter in position for sixth gear")
+                        break
 
 # Selection for seventh gear
 seven_gears = ''
@@ -199,7 +192,6 @@ if seven_gears == 'y' or seven_gears == 'yes':
     seven_gears = True
     done = False
     while not done:
-        # Event processing step.
         for event in pygame.event.get():
             if event.type == pygame.JOYBUTTONDOWN:
                 for i in range(num_buttons):
@@ -214,27 +206,26 @@ if seven_gears == 'y' or seven_gears == 'yes':
                             print("Put the shifter in position for seventh gear")
                             break
 else:
-    seven_gears = False                        
+    seven_gears = False
 
 # Selection for reverse
 print("Put the shifter in position for reverse")
 done = False
 while not done:
-        # Event processing step.
-        for event in pygame.event.get():
-            if event.type == pygame.JOYBUTTONDOWN:
-                for i in range(num_buttons):
-                    if shifter.get_button(i) == True:
-                        if i not in reserved:
-                            reverse = i
-                            reserved.append(i)
-                            print(f"Button {reverse} saved for reverse")
-                            done = True
-                        else:
-                            print(f"Conflict detected button already in use: {reserved}")
-                            print("Put the shifter in position for reverse")
-                            break                                                           
-            
+    for event in pygame.event.get():
+        if event.type == pygame.JOYBUTTONDOWN:
+            for i in range(num_buttons):
+                if shifter.get_button(i) == True:
+                    if i not in reserved:
+                        reverse = i
+                        reserved.append(i)
+                        print(f"Button {reverse} saved for reverse")
+                        done = True
+                    else:
+                        print(f"Conflict detected button already in use: {reserved}")
+                        print("Put the shifter in position for reverse")
+                        break
+
 # Selection for up, down and neutral keys with input security checks
 up = 'F'
 while len(up) != 1 or ((ord(up) < 97 or ord(up) > 122) and (ord(up) < 48 or ord(up) > 57)):
@@ -243,7 +234,7 @@ while len(up) != 1 or ((ord(up) < 97 or ord(up) > 122) and (ord(up) < 48 or ord(
 down = 'R'
 while len(down) != 1 or ((ord(down) < 97 or ord(down) > 122) and (ord(down) < 48 or ord(down) > 57)) or down == up:
     down = input("Wich key do you want to be pressed for downshifts?: ")
-    down = down.lower()    
+    down = down.lower()
 neut_key = 'F'
 while len(neut_key) != 1 or ((ord(neut_key) < 97 or ord(neut_key) > 122) and (ord(neut_key) < 48 or ord(neut_key) > 57)) or neut_key == up or neut_key == down:
     neut_key = input("Wich key do you want to use for neutral?: ")
@@ -269,27 +260,27 @@ else:
 config = configparser.ConfigParser()
 
 # Set configuration
-config['SHIFTER'] = {'Joystick id': joy,                   
-                   'first gear': first_gear,
-                   'second gear': second_gear,
-                   'third gear': third_gear,
-                   'fourth gear': fourth_gear,
-                   'fifth gear': fifth_gear,
-                   'sixth gear': sixth_gear,
-                   'seventh gear': seventh_gear,
-                   'reverse button': reverse                   
-                   }
+config['SHIFTER'] = {'Joystick id': joy,
+                     'first gear': first_gear,
+                     'second gear': second_gear,
+                     'third gear': third_gear,
+                     'fourth gear': fourth_gear,
+                     'fifth gear': fifth_gear,
+                     'sixth gear': sixth_gear,
+                     'seventh gear': seventh_gear,
+                     'reverse button': reverse
+                     }
 
 config['KEYS'] = {'upshift': upshift,
-                'downshift': downshift,
-                'neutral keyboard key': neut_key
-                 }   
+                  'downshift': downshift,
+                  'neutral keyboard key': neut_key
+                  }
 
 config['OPTIONS'] = {'seven gears': seven_gears,
-                    'neutral detection': neutral,
-                    'presskey timer': 0.2,  
-                    'releasekey timer': 0.5
-                    }
+                     'neutral detection': neutral,
+                     'presskey timer': 0.2,
+                     'releasekey timer': 0.5
+                     }
 
 # Write the file
 with open("Anyshift.ini", "w") as configfile:
