2b236b80ae6655083b1d86ce5da70a2c88f5bf76
unknown <bertoalarcon@gmail.com>
2023-05-17 17:28:16 +0200

    Cleaned repository structure

--- window.py
+++ window.py
@@ -1,11 +1,21 @@
 import tkinter
 from tkinter import ttk
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+import pygame
 import pygame
 import configparser
 import keyboard
 
+
 def save_data():
-    joy = joystick_id_combobox.get()
+    
+    active_joystick = joystick_id_combobox.get()
+    for i in range(num_joy):
+        if joys[i] == active_joystick:
+            active_joystick_id = i
+
+    joy = active_joystick_id
     first_gear = first_gear_entry.get()
     second_gear = second_gear_entry.get()
     third_gear = third_gear_entry.get()
@@ -13,7 +23,7 @@ def save_data():
     fifth_gear = fifth_gear_entry.get() 
     sixth_gear = sixth_gear_entry.get()
     seventh_gear = seventh_gear_entry.get()
-    reverse = reverse_gear_entry.get()
+    reverse = reverse_gear_entry.get()    
     upshift = upshift_key_entry.get()
     neut_key = neutral_key_entry.get()
     downshift = downshift_key_entry.get()
@@ -52,7 +62,6 @@ def save_data():
 
 def joystick_activation():
 
-
     active_joystick = joystick_id_combobox.get()
     for i in range(num_joy):
         if joys[i] == active_joystick:
@@ -64,14 +73,15 @@ def joystick_activation():
 
     done = False
     while not done:
-        # Event processing step.
-        for event in pygame.event.get():
-            if event.type == pygame.JOYBUTTONDOWN:
-                for i in range(num_buttons):
-                    if shifter.get_button(i) == True:
-                        keyboard.press_and_release(i)                        
-                        
-              
+        if shifter.get_button(i) == True:
+            keyboard.press_and_release(i)  
+
+                                          
+def run_any():
+    
+    mypath = os.path.abspath(os.path.dirname(__file__))
+    os.system( f'"{mypath}/anyshift.exe"' )    
+         
 
 # Get list of joystick ids and save them into joys list
 pygame.joystick.init()
@@ -85,19 +95,22 @@ for i in range(num_joy):
 
 
 window = tkinter.Tk()
-window.title("Anyshift Config Tool")
+window.title("Anyshift Config")
 
 frame = tkinter.Frame(window)
 frame.pack()
 
+# Joystick selection
+joystick_frame = tkinter.LabelFrame(frame, text = "Joystick id")
+joystick_frame.grid(row = 0, column = 0, padx= 20, pady = 5)
+
+joystick_id_combobox = ttk.Combobox(joystick_frame, values = joys)
+joystick_id_combobox.grid(row = 0, column = 0)
+
+
 # Joystick buttons selection
 gears_selection_frame = tkinter.LabelFrame(frame, text = "Joystick Buttons")
-gears_selection_frame.grid(row = 0, column = 0, padx= 20, pady = 20)
-
-joystick_id_label = tkinter.Label(gears_selection_frame, text = "Joystick id")
-joystick_id_combobox = ttk.Combobox(gears_selection_frame, values = joys)
-joystick_id_label.grid(row = 0, column = 0)
-joystick_id_combobox.grid(row = 1, column = 0)
+gears_selection_frame.grid(row = 1, column = 0, padx= 20, pady = 5)
 
 # Select joystick button
 joy_button = tkinter.Button(gears_selection_frame, text = "Activate selected joystick", command = joystick_activation)
@@ -105,73 +118,73 @@ joy_button.grid(row = 0, column = 1)
 
 first_gear_label = tkinter.Label(gears_selection_frame, text = "1")
 first_gear_label.grid(row = 2, column = 0)
-first_gear_entry = tkinter.Entry(gears_selection_frame)
+first_gear_entry = tkinter.Entry(gears_selection_frame, width= 2)
 first_gear_entry.grid(row = 3, column = 0)
 
 
 second_gear_label = tkinter.Label(gears_selection_frame, text = "2")
 second_gear_label.grid(row = 4, column = 0)
-second_gear_entry = tkinter.Entry(gears_selection_frame)
+second_gear_entry = tkinter.Entry(gears_selection_frame, width= 2)
 second_gear_entry.grid(row = 5, column = 0)
 
 
 third_gear_label = tkinter.Label(gears_selection_frame, text = "3")
 third_gear_label.grid(row = 2, column = 1)
-third_gear_entry = tkinter.Entry(gears_selection_frame)
+third_gear_entry = tkinter.Entry(gears_selection_frame, width= 2)
 third_gear_entry.grid(row = 3, column = 1)
 
 
 fourth_gear_label = tkinter.Label(gears_selection_frame, text = "4")
 fourth_gear_label.grid(row = 4, column = 1)
-fourth_gear_entry = tkinter.Entry(gears_selection_frame)
+fourth_gear_entry = tkinter.Entry(gears_selection_frame, width= 2)
 fourth_gear_entry.grid(row = 5, column = 1)
 
 
 fifth_gear_label = tkinter.Label(gears_selection_frame, text = "5")
 fifth_gear_label.grid(row = 2, column = 2)
-fifth_gear_entry = tkinter.Entry(gears_selection_frame)
+fifth_gear_entry = tkinter.Entry(gears_selection_frame, width= 2)
 fifth_gear_entry.grid(row = 3, column = 2)
 
 
 sixth_gear_label = tkinter.Label(gears_selection_frame, text = "6")
 sixth_gear_label.grid(row = 4, column = 2)
-sixth_gear_entry = tkinter.Entry(gears_selection_frame)
+sixth_gear_entry = tkinter.Entry(gears_selection_frame, width= 2)
 sixth_gear_entry.grid(row = 5, column = 2)
 
 
 seventh_gear_label = tkinter.Label(gears_selection_frame, text = "7")
 seventh_gear_label.grid(row = 2, column = 3)
-seventh_gear_entry = tkinter.Entry(gears_selection_frame)
+seventh_gear_entry = tkinter.Entry(gears_selection_frame, width= 2)
 seventh_gear_entry.grid(row = 3, column = 3)
 
 
 reverse_gear_label = tkinter.Label(gears_selection_frame, text = "R")
 reverse_gear_label.grid(row = 4, column = 3)
-reverse_gear_entry = tkinter.Entry(gears_selection_frame)
+reverse_gear_entry = tkinter.Entry(gears_selection_frame, width= 2)
 reverse_gear_entry.grid(row = 5, column = 3)
 
 
-for widget in gears_selection_frame.winfo_children():
-    widget.grid_configure(padx = 10, pady = 5)
+#for widget in gears_selection_frame.winfo_children():
+#    widget.grid_configure(padx = 10, pady = 5)
 
 # Keys selection
 
 keys_selection_frame = tkinter.LabelFrame(frame, text = "Key Selection")
-keys_selection_frame.grid(row = 1, column = 1, padx= 20, pady = 20)
+keys_selection_frame.grid(row = 2, column = 0, padx= 20, pady = 5)
 
 upshift_key_label = tkinter.Label(keys_selection_frame, text = "Upshift")
 upshift_key_label.grid(row = 0, column = 0)
-upshift_key_entry = tkinter.Entry(keys_selection_frame)
+upshift_key_entry = tkinter.Entry(keys_selection_frame, width= 2)
 upshift_key_entry.grid(row = 0, column = 1)
 
 neutral_key_label = tkinter.Label(keys_selection_frame, text = "Neutral")
 neutral_key_label.grid(row = 1, column = 0)
-neutral_key_entry = tkinter.Entry(keys_selection_frame)
+neutral_key_entry = tkinter.Entry(keys_selection_frame, width= 2)
 neutral_key_entry.grid(row = 1, column = 1)
 
 downshift_key_label = tkinter.Label(keys_selection_frame, text = "Downshift")
 downshift_key_label.grid(row = 2, column = 0)
-downshift_key_entry = tkinter.Entry(keys_selection_frame)
+downshift_key_entry = tkinter.Entry(keys_selection_frame, width= 2)
 downshift_key_entry.grid(row = 2, column = 1)
 
 for widget in keys_selection_frame.winfo_children():
@@ -180,7 +193,7 @@ for widget in keys_selection_frame.winfo_children():
 # Options
 
 options_selection_frame = tkinter.LabelFrame(frame, text = "Options")
-options_selection_frame.grid(row = 1, column = 0, padx = 20, pady = 20)
+options_selection_frame.grid(row = 3, column = 0, padx = 20, pady = 5)
 
 neutral_var = tkinter.StringVar(value="False")
 neutral_check = tkinter.Checkbutton(options_selection_frame, text= "Detect Neutral",
@@ -194,7 +207,11 @@ seven_check.grid(row=1, column=0)
 
 # Save data button
 button = tkinter.Button(frame, text="Save config", command= save_data)
-button.grid(row=2, column=0, sticky="news", padx=20, pady=10)
+button.grid(row=4, column=0, sticky="news", padx=20, pady = 5)
 
-pygame.init() 
+# Run Button
+run_button = tkinter.Button(frame, text="Run Anyshift", command= run_any)
+run_button.grid(row=5, column=0, sticky="news", padx=20, pady = 5)
+
+
 window.mainloop()
